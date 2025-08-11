32b667dd6b3096e18baa5eb1d3772f37a9db317e
Lee <1527778263@qq.com>
2025-08-11 23:43:29 +0800

    写注释

--- src/image.py
+++ src/image.py
@@ -1,2 +1,19 @@
-#系统函数容易忘记，如果把系统函数封装起来实现我们自己的类，
+# 系统函数容易忘记，如果把系统函数封装起来实现我们自己的类，
 # 并且加入一些定制化功能，那么调用起来一定会更加方便
+import pygame
+
+# 继承自 Sprite 类可以让你使用 Pygame 的Group功能
+# 方便管理游戏中的多个对象
+class Image(pygame.sprite.Sprite):
+    # __init__ 是类的构造函数，当创建 Image 对象时，会自动运行
+    # self 代表当前创建的对象本身
+    def __init__(self,path):
+        # 将传入的路径保存到 self.path 属性中，以便后续使用
+        self.path = path
+
+        self.image = pygame.image.load(self.path)
+
+    def draw(self,ds):
+        # ds.blit() 函数把 self.image 绘制到 ds 上
+        # ds为self.image.get_rect() 获取到的图片矩形区域
+        ds.blit(self.image,self.image.get_rect())
--- src/main.py
+++ src/main.py
@@ -1,19 +1,40 @@
 # 主窗口
+# 导入主要工具pygame库
 import pygame
+# 导入所有常量，如QUIT
 from pygame.locals import *
+# 导入sys库，用于与Python解释器交互
 import sys
+
+# 初始化pygame所有模块
 pygame.init()
 
-DS = pygame.display.set_mode((1280 , 600))  # 创建窗口
+# 创建一个画布：一个指定尺寸的窗口，并返回一个 Surface 对象
+DS = pygame.display.set_mode((1280 , 600))
 
-image = pygame.image.load('../pic/other/back.png')
+# 导入自定义的image模块
+import image
+
+# 使用自定义的image类导入背景图片
+# image = pygame.image.load('../pic/other/back.png')
+img = image.Image('../pic/other/back.png')
+
+# 游戏主循环
 while True:
+    # 遍历事件队列，获取所有发生的事件（比如鼠标点击、键盘输入、关闭窗口等）
     for event in pygame.event.get():
+        # 检测事件类型
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
+    # 用白色 (255,255,255) 填充整个窗口
     DS.fill((255,255,255))
-    DS.blit(image,image.get_rect())
+    # DS.blit(image,image.get_rect())
+
+    # 调用自定义 Image 类的 draw 方法，把图片绘制到 DS 这个窗口上
+    img.draw(DS)
+
+    # 更新整个窗口的显示内容，把上面绘制的图片显示出来
     pygame.display.update()
 
